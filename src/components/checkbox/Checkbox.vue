a49cf58903a1c2ef5513f489ef060cf71a0a5ffc
Julyme3 <jg0311@bk.ru>
2023-03-04 19:41:19 +0500

    fix styles for checkbox component

--- src/components/checkbox/Checkbox.vue
+++ src/components/checkbox/Checkbox.vue
@@ -52,14 +52,18 @@ const localState = computed({
     align-items: center;
     user-select: none;
   }
-  &:checked + .checkbox-mark:after {
-    content: '';
+  &:checked + .checkbox-mark {
+    box-shadow: 0px 7px 20px rgba(0, 0, 0, 0.07);
+
+    &:after {
+      content: '';
+    }
   }
   &-mark {
     position: relative;
     height: 24px;
     width: 24px;
-    border: 1px solid #adb5bd;
+    border: 1px solid var(--grey);
     border-radius: 6px;
     background-color: #fff;
 
@@ -70,14 +74,15 @@ const localState = computed({
       top: 3px;
       width: 5px;
       height: 10px;
-      border: solid #6979f8;
+      border: solid var(--blue);
       border-width: 0 3px 3px 0;
       transform: rotate(45deg);
     }
   }
 
   &:disabled + .checkbox-mark {
-    background-color: #e9ecef;
+    background-color: var(--grey-dark);
+    border-color: var(--grey-dark);
   }
 }
 </style>
