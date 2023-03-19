12b06bb2eb746c8819e76e9e082e10eb9de6167e
Julyme3 <jg0311@bk.ru>
2023-03-19 22:19:09 +0300

    styles for Input component

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -1,10 +1,81 @@
 <template>
   <div class="input-container">
-    <input type="text" class="input" />
-    <label class="input-label"></label>
+    <input
+      :type="type"
+      class="input-text"
+      :value="value"
+      :placeholder="placeholder"
+      :name="name"
+      :id="name"
+    />
+    <label v-if="label" :for="name" class="input-label">{{ label }}</label>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+  type: String
+  label?: String
+  value: String
+  name: String
+  placeholder?: String
+}>()
+</script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.input {
+  &-container {
+    margin-bottom: 30px;
+    position: relative;
+  }
+  &-error {
+    background: var(--danger);
+    border-radius: 7px;
+    font-size: 13px;
+    color: #fff;
+    padding: 5px;
+  }
+  &-text {
+    position: relative;
+    padding: 0 10px;
+    height: 40px;
+    font-size: 15px;
+    width: 100%;
+    z-index: 1;
+    border: 1px solid var(--primary);
+    border-radius: 7px;
+
+    &:not(:placeholder-shown) + .input-label {
+      z-index: 1;
+      opacity: 1;
+      top: -20px;
+    }
+
+    &:focus + .input-label {
+      z-index: 1;
+      opacity: 1;
+      top: -20px;
+    }
+  }
+  &-label {
+    display: block;
+    position: absolute;
+    top: 20px;
+    opacity: 0;
+    z-index: -1;
+    transition: opaity 0.3s, top 0.3s;
+    font-size: 13px;
+    color: var(--primary);
+    font-weight: bold;
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
--- src/views/InputView.vue
+++ src/views/InputView.vue
@@ -1,10 +1,19 @@
 <template>
   <h1 class="heading-1">Input</h1>
-  <Input />
+  <Input
+    name="Your name"
+    label="Your name"
+    :value="nameField"
+    type="text"
+    placeholder="Input your name"
+  />
 </template>
 
 <script setup lang="ts">
 import Input from '@/components/input/Input.vue'
+import { ref } from 'vue'
+
+const nameField = ref('')
 </script>
 
 <style scoped></style>
